9746a6e1cd20b992ae2d95b460b814732444e130
jamesxddev <jamescabral.dev@gmail.com>
2025-04-04 17:46:52 +0800

    added header image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -36,7 +36,7 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/mp-header-logo.jpg')}
           style={styles.reactLogo}
         />
       }>
@@ -45,15 +45,13 @@ export default function HomeScreen() {
       </ThemedView>
       
       <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
-        <View style={styles.buttonContainer}>
-        </View>
-      </CameraView>
-    </View>
+        {/* <CameraView style={styles.camera} facing={facing}>
+          <View style={styles.buttonContainer}>
+          </View>
+        </CameraView> */}
+      </View>
+
       <ThemedView style={styles.stepContainer}>
-
-        
-
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
@@ -109,8 +107,8 @@ const styles = StyleSheet.create({
     margin: 100,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
+    height: 200,
+    width: 430,
     bottom: 0,
     left: 0,
     position: 'absolute',
--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -11,7 +11,7 @@ import { ThemedView } from '@/components/ThemedView';
 import { useBottomTabOverflow } from '@/components/ui/TabBarBackground';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
-const HEADER_HEIGHT = 250;
+const HEADER_HEIGHT = 200;
 
 type Props = PropsWithChildren<{
   headerImage: ReactElement;
@@ -54,7 +54,6 @@ export default function ParallaxScrollView({
         <Animated.View
           style={[
             styles.header,
-            { backgroundColor: headerBackgroundColor[colorScheme] },
             headerAnimatedStyle,
           ]}>
           {headerImage}
