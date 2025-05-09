6bf31a077079b43249b923b316f015de819ae4ef
jamesxddev <jamescabral.dev@gmail.com>
2025-05-09 14:11:26 +0800

    updated fix upgrade errors

--- config.ts
+++ config.ts
@@ -4,15 +4,15 @@ const ENV = (process.env.NODE_ENV as Env) || 'development';
 
 const ENV_CONFIG: Record<Env, { API_URL: string }> = {
   development: {
-    API_URL: 'https://localhost:44305/api', // web
+    // API_URL: 'https://localhost:44305/api', // web
     // API_URL: 'http://192.168.100.27:5000/api', // mobile
-    // API_URL: 'http://192.168.100.27:8082/api', // IIS 
+    API_URL: 'http://192.168.100.27:8082/api', // IIS 
   },
   production: {
-    API_URL: 'https://api.myproduction.com',
+    API_URL: 'http://192.168.1.100:8082/api'
   },
   test: {
-    API_URL: 'https://api.mytestenv.com',
+    API_URL: 'http://192.168.100.27:8082/api'
   },
 };
 
